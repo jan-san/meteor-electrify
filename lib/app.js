jan-san/meteor-electrify

commit 494f26fac3397a5484a6403bd66ad55c405f3f21

fixed/improved check for package version in app.js (previously checked for the old package name `electrify` and only as non-dev dependency)

lib/app.js
@@ -186,16 +186,19 @@ App.prototype.ensure_deps = function(done) {
   else {
     // fetch version infos
     var pkg_path    = join(this.$.env.app.root, 'package.json');
-    var pkg_version = require(pkg_path).dependencies.electrify;
+    var config      = require(pkg_path);
+    var pkg_version = _.get(config, "devDependencies['meteor-electrify']") || _.get(config, "dependencies['meteor-electrify']");
     var version     = this.$.env.version;
 
     // check and warn users about electrify versions mismatch
     if(pkg_version != version) {
       this.log.warn([
-        "{{IMPORTANT}} Your local meteor-electrify npm package must match version",
-        version,
-        " -- please update it to avoid errors"
-      ].join(' '));
+        "{{IMPORTANT}} Your local meteor-electrify npm package version (",
+        pkg_version,
+        ") must match version (",
+        pkg_version,
+        ") -- please update it to avoid errors"
+      ].join(''));
     }
 
     spawn(npm_cmd, ['i'], {

commit 12c358b137351c6201de217f3ac8dfbeccc5f6e6

fixed minor error introduced in 494f26fac3397a5484a6403bd66ad55c405f3f21

lib/app.js
@@ -196,7 +196,7 @@ App.prototype.ensure_deps = function(done) {
         "{{IMPORTANT}} Your local meteor-electrify npm package version (",
         pkg_version,
         ") must match version (",
-        pkg_version,
+        version,
         ") -- please update it to avoid errors"
       ].join(''));
     }